628d82152b4492c10646a6aa74d53e629b22b8d6
vLia <vLia@users.noreply.github.com>
2016-07-28 11:44:08 +0300

    Modified default timeout interval for Jasmine

--- scripts/protractor/conf.js
+++ scripts/protractor/conf.js
@@ -2,6 +2,6 @@
 exports.config = {
     framework: 'jasmine',
     specs: ['spec.js'],
-    allScriptsTimeout: 600000,
-    jasmineNodeOpts: {defaultTimeoutInterval: 600000}
+    allScriptsTimeout: 1200000,
+    jasmineNodeOpts: {defaultTimeoutInterval: 1200000}
 };
